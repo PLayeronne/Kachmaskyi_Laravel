6d99ffc11c2d68660be10f59e6fb6d8bddb49a31
PLayeronne <155058043+PLayeronne@users.noreply.github.com>
2024-06-20 01:25:37 +0300

    lab 7 done

--- blog/app/Http/Controllers/Blog/Admin/PostController.php
+++ blog/app/Http/Controllers/Blog/Admin/PostController.php
@@ -8,11 +8,23 @@ use Illuminate\Http\Request;
 class PostController extends BaseController
 {
     /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->blogPostRepository = app(BlogPostRepository::class); //app вертає об'єкт класа
+    }
+    /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $paginator = $this->blogPostRepository->getAllWithPaginate();
+
+        return view('blog.admin.posts.index', compact('paginator'));
     }
 
     /**
